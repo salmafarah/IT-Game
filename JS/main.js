9412dc86da77a8e8fb54e9a8e46fc9f65c15a62a
Salma Farah <salmafarah08@gmail.com>
2020-03-04 10:20:26 -0500

    added all the questions

--- JS/main.js
+++ JS/main.js
@@ -6,18 +6,18 @@ const QUESTIONS = [
         image: "https://imgur.com/qgczFSr.jpg", 
         q:  "What was Roy's catch pharse on the show?", 
         answers:[ "Make it so", "Have you tried turning it on and off again.", "You shall not pass", "Live long and prosper."], 
-        correct: 1, 
+        correct: 2, 
     }, 
     {
         image: "https://imgur.com/PUozVHR.jpg", 
         q: "Why did Phillip ask Jen out to the musical?", 
         answers: ["She looks like a man.", "She loves musicals.", "He wanted to hangout with Moss and Roy.", "It was a work outing."], 
-        correct: 2, 
+        correct: 1, 
     },
     {
         image: "https://imgur.com/frXfz6X.jpg", 
         q: "What happened to Moss when he wanted to learn German cuisine?", 
-        answers: ["He didn't like the food.", "He's instructor was a cannibal", "He loved it.", "German food just wasn't for him"],
+        answers: ["He wasn't a chef.", "His instructor was a cannibal", "He burned the building down and couldn't call 999.", "German food just wasn't for him"],
         correct: 3, 
     },
     { 
@@ -25,7 +25,49 @@ const QUESTIONS = [
         q: "What did Roy's date think was on his forehead?", 
         answers: ["Chocolate", "Poop", "He's self-respect", "Computer Gunk"], 
         correct: 4, 
-    }
+    },
+    { 
+        image: "https://imgur.com/BvMKE5y.jpg", 
+        q: "What was the word that won Moss the Countdown teapot.", 
+        answers: ["Promiscuous", "Mandem", "Tnetennba", "Vista"], 
+        correct: 3, 
+    },
+    {
+        image: "https://imgur.com/cS6gFg2.jpg", 
+        q: "What a bunch of  ", 
+        answers: ["Cows", "Arseholed", "Nimrods", "Bastards"], 
+        correct: 4, 
+    }, 
+    {
+        image: "https://imgur.com/eivqY5R.jpg", 
+        q: "What happened to Roy's girlfriends parents at the Sea Parks?", 
+        answers: ["They were murdered.", "They died in a fire.", "They got married.", "They've been stuck there for 30 years."], 
+        correct: 2, 
+    }, 
+    {
+        image: "https://imgur.com/b5ruE4y.jpg", 
+        q: "What was Jen's boyfriends name?", 
+        answers: ["Peter File", "File Peter", "Pedo File", "Mister Love"], 
+        correct: 1, 
+    }, 
+    {
+        image: "https://imgur.com/y77aH93.jpg", 
+        q: "What is the new emergency number changed too?", 
+        answers: ["0018-999-881-999-119-725-3", "999", "Dear Sir stroke Madam", "0118-999-881-999-119-725-3"], 
+        correct: 4, 
+    },     
+    {
+        image: "https://imgur.com/WU41tqa.jpg", 
+        q: "Why was Richmond demoted ?", 
+        answers: ["He was bad at his job.", "He become goth.", "He was found laundering money.", "He was put into witness protection."], 
+        correct: 2, 
+    }, 
+    {
+        image: "https://imgur.com/hODJxS7.jpg", 
+        q: "Where did Moss and Roy take the sex workers?", 
+        answers: ["To Amsterdam", "To a carnival", "To a cruise ship", "To the elders of the internet."], 
+        correct: 2, 
+    }, 
 ]
 
 
@@ -41,38 +83,36 @@ let buttons= document.querySelectorAll(".button");
 let buttonEl= document.getElementById("buttons");
 let prevBtnEl= document.getElementById("prev-btn");
 let nextBtnEl= document.getElementById("next-btn");
-let startEl= document.getElementsById("start-btn");  
+let startEl= document.getElementById("start-btn");  
 let modalEl= document.getElementById("modal"); 
 
 
-
-// images.src = QUESTIONS[sorting()].image; 
-// qbox = QUESTIONS[sorting()].q; 
-// buttons = QUESTIONS[0].answers[0];
 
   
 /*----- event listeners -----*/
 nextBtnEl.addEventListener('click', nextQuestion)
 prevBtnEl.addEventListener('click', prevQuestion)
 buttonEl.addEventListener('click',correctAnswer)
-startEl.addEventListener('click', start) 
+// startEl.addEventListener('click', start) 
 
 
 /*----- functions -----*/
 
 init() 
 
-function off(event){
-    if(event.target.value == startEl){
-        modalEl.style.display= "none"; 
-    }
-}
-function start(event){
-    if(event)
+// function off(event){
+//     if(event.target.value == startEl){
+//         modalEl.style.display= "none"; 
+//     }
+// }
+
+
+/ window.onload(); 
+window.onload = function start(){
+   
 }
 
 
-window.onload(); 
 
 
 
@@ -127,8 +167,9 @@ function prevQuestion() {
 
 function init() {
   
-    currQuestion = 0
+    currQuestion = 0; 
     render(); 
+    start(); 
 }
 
 function render() {

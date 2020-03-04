c8a9b6c3bda34b2f67bb68cabf09c1d1fc1d9aa9
Salma Farah <salmafarah08@gmail.com>
2020-03-03 20:04:26 -0500

    add the feature for the buttons

--- JS/main.js
+++ JS/main.js
@@ -1,25 +1,30 @@
 
 /*----- constants -----*/
+
 const QUESTIONS = [
     {
         image: "https://imgur.com/qgczFSr.jpg", 
         q:  "What was Roy's catch pharse on the show?", 
         answers:[ "Make it so", "Have you tried turning it on and off again.", "You shall not pass", "Live long and prosper."], 
+        correct: 1, 
     }, 
     {
         image: "https://imgur.com/PUozVHR.jpg", 
         q: "Why did Phillip ask Jen out to the musical?", 
         answers: ["She looks like a man.", "She loves musicals.", "He wanted to hangout with Moss and Roy.", "It was a work outing."], 
+        correct: 2, 
     },
     {
         image: "https://imgur.com/frXfz6X.jpg", 
         q: "What happened to Moss when he wanted to learn German cuisine?", 
         answers: ["He didn't like the food.", "He's instructor was a cannibal", "He loved it.", "German food just wasn't for him"],
+        correct: 3, 
     },
     { 
         image: "https://imgur.com/VyXvUmz.jpg", 
         q: "What did Roy's date think was on his forehead?", 
         answers: ["Chocolate", "Poop", "He's self-respect", "Computer Gunk"], 
+        correct: 4, 
     }
 ]
 
@@ -30,11 +35,16 @@ let currQuestion;
 
 /*----- cached element references -----*/
 
-let imageEl = document.getElementById("images"); 
-let qboxEl = document.getElementById("qbox"); 
-let buttons = document.querySelectorAll(".button");
-let prevBtnEl = document.getElementById('prev-btn');
-let nextBtnEl = document.getElementById('next-btn');
+let imageEl= document.getElementById("images"); 
+let qboxEl= document.getElementById("qbox"); 
+let buttons= document.querySelectorAll(".button");
+let buttonEl= document.getElementById("buttons");
+let prevBtnEl= document.getElementById("prev-btn");
+let nextBtnEl= document.getElementById("next-btn");
+let startEl= document.getElementsById("start-btn");  
+let modalEl= document.getElementById("modal"); 
+
+
 
 // images.src = QUESTIONS[sorting()].image; 
 // qbox = QUESTIONS[sorting()].q; 
@@ -44,15 +54,70 @@ let nextBtnEl = document.getElementById('next-btn');
 /*----- event listeners -----*/
 nextBtnEl.addEventListener('click', nextQuestion)
 prevBtnEl.addEventListener('click', prevQuestion)
+buttonEl.addEventListener('click',correctAnswer)
+startEl.addEventListener('click', start) 
+
 
 /*----- functions -----*/
 
 init() 
 
+function off(event){
+    if(event.target.value == startEl){
+        modalEl.style.display= "none"; 
+    }
+}
+function start(event){
+    if(event)
+}
+
+
+window.onload(); 
+
+
+
+
+
+function correctAnswer(event){
+   if(event.target.id == "buttons"){
+       return; 
+   }
+    if(event.target.value == QUESTIONS[currQuestion].correct){
+        event.target.classList.add("success")
+        buttons.forEach(function(cb){
+            cb.disabled = true;
+         }); 
+         event.target.disabled = false
+    } else{
+        event.target.classList.add("fail")
+        }; 
+
+}
+
+
+
+    //     event.target.style.backgroundColor = 'green'; 
+    //     buttonEl.removeEventListener('click',correctAnswer
+    // } 
+    // buttons.forEach(function(cb){
+    //     cb.classList.add("success", "fail"); 
+    // }); 
+   
+
+
+
 function nextQuestion() {
     currQuestion++
-    render()
+    render(); 
+    
+        buttons.forEach(function(cb) {
+            cb.classList.remove("success")
+            cb.classList.remove("fail")
+            cb.disabled = false;
+        }); 
+         
 }
+
 
 function prevQuestion() {
     currQuestion--
@@ -61,18 +126,20 @@ function prevQuestion() {
 
 
 function init() {
+  
     currQuestion = 0
-    render()
+    render(); 
 }
 
 function render() {
     imageEl.src = QUESTIONS[currQuestion].image;
     qbox.textContent = QUESTIONS[currQuestion].q;
     for (let i = 0; i < buttons.length; i++){
-        buttons[i].textContent = QUESTIONS[currQuestion].answers[i]
+        buttons[i].textContent= QUESTIONS[currQuestion].answers[i]; 
     }
     prevBtnEl.style.display = currQuestion === 0 ? 'none' : 'inline'
     nextBtnEl.style.display = currQuestion === QUESTIONS.length - 1 ? 'none' : 'inline'
+
 }
 
 
